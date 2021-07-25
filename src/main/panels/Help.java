93c897e315f23ef54ac77ba0393f182a221753f5
exoad <error666jm@gmail.com>
2021-07-25 12:35:50 -0400

    append

---
+++ src/main/panels/Help.java
@@ -0,0 +1,52 @@
+package src.main.panels;
+
+/*
+ * TODO: Add better help menu
+ */
+import java.awt.Component;
+import java.awt.Dimension;
+
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+/*
+ * Awaiting Packages
+ * import java.io.*;
+ * import java.util.*;
+ * import java.awt.event.*;
+ */
+
+public class Help extends JPanel implements Runnable {
+    private final JFrame frame;
+
+    public Help() {
+        frame = new JFrame("Help Menu");
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+
+        JLabel title = new JLabel("--Helpful Handbook--");
+        title.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        JButton readHelpAsTXT = new JButton("Read As TxT");
+        readHelpAsTXT.setAlignmentY(Component.BOTTOM_ALIGNMENT);
+
+        setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+        setPreferredSize(new Dimension(300, 100));
+
+        add(title);
+        add(readHelpAsTXT);
+
+        frame.add(this);
+    }
+    @Override
+    public void run() {
+        frame.pack();
+        frame.setVisible(true);
+    }
+    public void askRun() {
+        new Help().run();
+    }
+    
+}
---
+++ src/main/schedulers/FilePathHandler.java
@@ -0,0 +1,12 @@
+package src.main.schedulers;
+
+public enum FilePathHandler {
+  SAVE_PATH("click_game/");
+  private final String val;
+  FilePathHandler(String t) {
+    this.val = t;
+  }
+  public String getPath() {
+    return this.val;
+  }
+}
