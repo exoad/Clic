d8d5edeca383c35e870719ea7cdf807f94b0f9f2
exoad <error666jm@gmail.com>
2021-07-29 14:19:23 -0400

    added licensing

--- src/main/Runner.java
+++ src/main/Runner.java
@@ -4,6 +4,37 @@
  * @author Jack Meng
  * @version 1.0z (EXP)
  * 
+ * BSD 3-Clause License
+ * 
+ * Copyright (c) 2021, Jack Meng
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ * 
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ * 
+ * 3. Neither the name of the copyright holder nor the names of its
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * 
+ * 
  */
 package src.main;
 
@@ -60,9 +91,10 @@ public class Runner extends JPanel implements ActionListener, Runnable {
     UPGRADEA.setAlignmentX(Component.CENTER_ALIGNMENT);
 
     // settings all the variables and components
-    if (Integer.parseInt(fsr.readLineNumber(0)) != 0 && Integer.parseInt(fsr.readLineNumber(1)) != 0 && Integer.parseInt(fsr.readLineNumber(2)) != 0 && Integer.parseInt(fsr.readLineNumber(3)) != 0) {
+    if (Integer.parseInt(fsr.readLineNumber(0)) != 0 && Integer.parseInt(fsr.readLineNumber(1)) != 0
+        && Integer.parseInt(fsr.readLineNumber(2)) != 0 && Integer.parseInt(fsr.readLineNumber(3)) != 0) {
       mainLabel = Integer.parseInt(fsr.readLineNumber(0));
-      if(mainLabel >= 100) 
+      if (mainLabel >= 100)
         UPGRADEA.setVisible(true);
       multX = Integer.parseInt(fsr.readLineNumber(1));
       objNum = Integer.parseInt(fsr.readLineNumber(2));
@@ -105,7 +137,7 @@ public class Runner extends JPanel implements ActionListener, Runnable {
     otherInfo.setVisible(true);
     otherInfo.setAlignmentX(Component.CENTER_ALIGNMENT);
 
-    SAVE_CLICK_IMG = new ImageIcon(ImageHandler.SAVE_BTN.getVal()); 
+    SAVE_CLICK_IMG = new ImageIcon(ImageHandler.SAVE_BTN.getVal());
 
     SAVX = new JButton("Save", SAVE_CLICK_IMG);
     SAVX.setBackground(Color.red);
@@ -213,8 +245,8 @@ public class Runner extends JPanel implements ActionListener, Runnable {
     } while (sr.checkYN(s) != -1);
     sc.close();
 
-    //changes the random blurb every X seconds
-    while(true) {
+    // changes the random blurb every X seconds
+    while (true) {
       Thread.sleep(2000);
       news.setText(new InfoBox().randomNews());
     }
